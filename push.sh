1b36547fc72db5831c1d1f9abfae33cf628f75a4
Mahmudul Hasan <mahmudulrm@gmail.com>
2017-02-17 05:41:32 +0600

    Update push.sh

--- push.sh
+++ push.sh
@@ -2,3 +2,23 @@
 git add -A
 git commit -m "Mahmudul"
 git push
+
+git init 
+git status
+git clone https://github.com/mahmudulrm/WorkPlace
+git add -A
+git commit -m "Mahmudul"
+git push
+
+---- create new branch ----
+git checkout -b [name_of_your_new_branch]
+---- same data copy ----
+git push origin [name_of_your_new_branch]
+
+--- list show---
+git branch
+
+--- change branch name ---
+git checkout
+
+git remote add [name_of_your_remote]
